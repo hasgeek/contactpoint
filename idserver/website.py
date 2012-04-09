b257d36179e29f3a7375bb09922e83ba0f464b2c
Kiran Jonnalagadda <jace@pobox.com>
2012-04-10 01:13:39 +0530

    Added error handlers and fixed a port.

--- idserver/website.py
+++ idserver/website.py
@@ -1,30 +1,37 @@
 from smartcard.Exceptions import NoCardException, CardConnectionException
+from smartcard.pcsc.PCSCExceptions import EstablishContextException
 from smartcard.System import readers
 from smartcard.util import toHexString
 
 from flask import Flask
 from coaster.views import jsonp
 
-GET_ID=[0xFF, 0xCA, 0x00, 0x00, 0x04] #Get Serial Number
+GET_ID = [0xFF, 0xCA, 0x00, 0x00, 0x04]  # Get serial number for Mifare NFC
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello, world!"
 
+
 @app.route('/card_id.json')
 def card_id():
-    for reader in readers():
-        try:
-            connection = reader.createConnection()
-            connection.connect()
-            response, sw1, sw2 = connection.transmit(GET_ID)
-            tag = toHexString(response).replace(" ","")
-            return jsonp(card_id=tag)
-        except (NoCardException, CardConnectionException):
-            return jsonp(card_id=None)
-    return jsonp(card_id=None)
+    try:
+        for reader in readers():
+            try:
+                connection = reader.createConnection()
+                connection.connect()
+                response, sw1, sw2 = connection.transmit(GET_ID)
+                tag = toHexString(response).replace(" ", "")
+                return jsonp(card_id=tag)
+            except (NoCardException, CardConnectionException):
+                return jsonp(card_id=None, error='no_card')
+    except EstablishContextException:
+        return jsonp(card_id=None, error='no_pcscd')
+    return jsonp(card_id=None, error='no_reader')
 
-if __name__=='__main__':
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8008, debug=False)
