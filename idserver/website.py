5e8ada963439a565c2b09580cf65a95ec5f2a632
Sajjad Anwar <me@sajjad.in>
2012-05-21 15:56:54 +0530

    Generate labels, incomplete print server

--- idserver/website.py
+++ idserver/website.py
@@ -6,10 +6,13 @@ from smartcard.util import toHexString
 from flask import Flask
 from coaster.views import jsonp
 
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import mm
+
+
 GET_ID = [0xFF, 0xCA, 0x00, 0x00, 0x04]  # Get serial number for Mifare NFC
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -41,5 +44,21 @@ def card_id():
     return response
 
 
+@app.route('/print/<name>/<twitter>', methods=['POST'])
+def label(name, twitter):
+    c = canvas.Canvas(name, pagesize=(90*mm, 29*mm))
+    c.drawCentredString(45*mm, 20*mm, name);
+    c.setFontSize(10)
+    c.drawCentredString(45*mm, 15*mm, twitter);
+    c.showPage()
+    try:
+        c.save()
+        return "success"
+    except:
+        return "fail"
+
+
+
+
 if __name__ == '__main__':
-    app.run('127.0.0.1', 8008, debug=False)
+    app.run('127.0.0.1', 8008, debug=True)
